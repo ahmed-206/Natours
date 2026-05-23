93c41e758e0f2713110439ff4624a7bbacd2b899
Ahmed Ramadan <elgen9961@gmail.com>
2026-05-23 14:26:43 +0300

    feat: initial project setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('Can post to this endpoint...');
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
